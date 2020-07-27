8063ef09964227329927e4fc461516de312cb4b3
BuildTools <unconfigured@null.spigotmc.org>
2020-07-27 15:11:06 +0200

    Affiche tous les joueurs ayant changer de tdc depuis le dernier relevé (ne prend en compte que les 20 premières pages du classement)

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from tracker import tracker
 from tui import connexion
 from time import time
@@ -5,5 +7,6 @@ from time import time
 if __name__ == "__main__":
     # connexion()
     start = time()
-    print(tracker.tdc())
+    with pd.option_context("display.max_columns", None, "display.width", 180):
+        print(tracker.compare())
     print(time()-start, "s")
--- tracker/tracker.py
+++ tracker/tracker.py
@@ -1,4 +1,6 @@
 from threading import Thread
+import os
+from datetime import datetime
 
 import pandas as pd
 import requests
@@ -6,14 +8,15 @@ from bs4 import BeautifulSoup
 
 from data import get_serveur
 
-N_PAGES = 30
+N_PAGES = 20
+COLUMNS = ("Pseudo", "Tdc", "Fourmilière", "Technologie", "Trophées", "Alliance")
 
 
 class TdcSaver(Thread):
     def __init__(self, page):
         Thread.__init__(self)
         self.page = str(page)
-        self.url = "http://" + get_serveur() + ".fourmizzz.fr/classement.php?page=" \
+        self.url = "http://" + get_serveur() + ".fourmizzz.fr/classement2.php?page=" \
                    + self.page + "&typeClassement=terrain"
 
     def run(self):
@@ -21,27 +24,58 @@ class TdcSaver(Thread):
         r = requests.get(self.url, cookies=cookies)
         soup = BeautifulSoup(r.text, "html.parser")
         table = soup.find("table", {"class": "tab_triable"})
-        df = pd.DataFrame(columns=["pseudo", "tdc"])
+        df = pd.DataFrame(columns=list(range(6)))
         for row in table.find_all("tr")[1:]:
-            df = df.append(pd.DataFrame({i: elem.text for i, elem in enumerate(row.find_all("td"))}, index=[0]),
-                           ignore_index=True)
+            elems = row.find_all("td")
+            elems = {i: elem for i, elem in enumerate(elems)}
+            if len(elems[1].find_all("a")) == 2:
+                pseudo, alliance = elems[1].find_all("a")
+                elems[1] = pseudo
+                elems[len(elems)] = alliance
+            df = df.append(pd.DataFrame({i: [elem.text] for i, elem in elems.items()}, index=[0]), ignore_index=True)
 
         df.drop(0, axis=1, inplace=True)
         df.dropna(axis=1, inplace=True)
-        df.columns = ["Alliance", "Pseudo", "Tdc"]
+        if len(df.columns) == 5:
+            df = pd.concat([df, pd.DataFrame(columns=["Alliance"])])
+        df.columns = COLUMNS
+        df["Tdc"] = [int(tdc.replace(" ", "")) for tdc in df["Tdc"]]
         df.to_pickle("tracker/tdc_temp/tdc_" + self.page)
 
 
-def tdc():
+def scrap_tdc():
     tdc_saver_lst = [TdcSaver(i) for i in range(1, N_PAGES+1)]
     for tdc_saver in tdc_saver_lst:
         tdc_saver.start()
     for i, tdc_saver in enumerate(tdc_saver_lst):
         tdc_saver.join()
 
-    df = pd.DataFrame(columns=["Alliance", "Pseudo", "Tdc"])
+
+def compare():
+    if not os.path.exists("tracker/tdc_temp/tdc_" + str(N_PAGES)):
+        scrap_tdc()
+
+    releve_1 = merge_files()
+    releve_1 = pd.DataFrame({row[0]: [row[1]] for index, row in releve_1.loc[:, ["Pseudo", "Tdc"]].iterrows()})
+    date = os.path.getmtime("tracker/tdc_temp/tdc_1")
+    date = datetime.fromtimestamp(date).replace(microsecond=0)
+    releve_1 = pd.concat([pd.DataFrame(dict(Date=[date])), releve_1], axis=1)
+
+    scrap_tdc()
+    releve_2 = merge_files()
+    releve_2 = pd.DataFrame({row[0]: [row[1]] for index, row in releve_2.loc[:, ["Pseudo", "Tdc"]].iterrows()})
+    releve_2 = pd.concat([pd.DataFrame(dict(Date=[datetime.now().replace(microsecond=0)])), releve_2], axis=1)
+
+    df = pd.concat([releve_1, releve_2], axis=0, ignore_index=True)
+    df = df.dropna(axis=1)
+    df = df.loc[:, (df != df.iloc[0]).any()]
+
+    return df
+
+
+def merge_files():
+    df = pd.DataFrame(columns=COLUMNS)
     for page in range(1, N_PAGES+1):
         df = df.append(pd.read_pickle("tracker/tdc_temp/tdc_" + str(page)), ignore_index=True)
 
     return df
-
